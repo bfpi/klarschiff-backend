e488f89598d9f872efb517e92df11f5a81d21a5c
ro3t <voss@bfpi.de>
2016-09-05 15:21:43 +0200

    import Column Annotation in Kategorie VO

--- src/main/java/de/fraunhofer/igd/klarschiff/vo/Kategorie.java
+++ src/main/java/de/fraunhofer/igd/klarschiff/vo/Kategorie.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
