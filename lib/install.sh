5e2777bc456aab2df970c067d4d03f25283634f0
ro3t <voss@bfpi.de>
2017-02-07 13:38:18 +0100

    Weka.jar nicht mehr über Repositories verfügbar

--- lib/install.sh
+++ lib/install.sh
@@ -8,3 +8,10 @@ mvn install:install-file \
   -DartifactId=commons-regex \
   -Dversion=0.1 \
   -Dpackaging=jar
+
+mvn install:install-file \
+  -Dfile=$DIR/weka-3.6.4.jar \
+  -DgroupId=weka \
+  -DartifactId=weka \
+  -Dversion=3.6.4 \
+  -Dpackaging=jar
